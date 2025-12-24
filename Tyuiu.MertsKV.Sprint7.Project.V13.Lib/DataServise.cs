bcaf25a84f4d3fe827173e37baa23f6f06770e9b
Кристина Мерц <Tyuiu.mertskv@outlook.com>
2025-12-24 15:28:23 +0500

    Add project files.

---
+++ Tyuiu.MertsKV.Sprint7.Project.V13.Lib/DataServise.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace Tyuiu.MertsKV.Sprint7.Project.V13.Lib
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public double Area { get; set; }
+        public int Population { get; set; }
+        public double Population_density { get; set; }
+        public string MainNationality { get; set; }
+        public bool IsDeveloped { get; set; }
+    }
+
+
+    public class DataService
+    {
+        public BindingList<Country> Countries { get; private set; }
+        public DataService()
+        {
+            Countries = new BindingList<Country>();
+        }
+
+        private BindingList<Country> originalCountries;
+
+
+        public void LoadFromCsv(string filePath)
+        {
+            Countries.Clear();
+            var lines = File.ReadAllLines(filePath, System.Text.Encoding.UTF8);
+            foreach (var line in lines.Skip(1))
+            {
+                var values = line.Split(';');
+                var country = new Country
+                {
+                    Name = values[0],
+                    Capital = values[1],
+                    Area = double.Parse(values[2]),
+                    Population = int.Parse(values[3]),
+                    Population_density = double.Parse(values[4]),
+                    MainNationality = values[5],
+                    IsDeveloped = bool.Parse(values[6]),
+                };
+                Countries.Add(country);
+            }
+        }
+
+
+
+        public void SaveToCsv(string filePath)
+        {
+            var lines = new List<string> {
+                "Название;Столица;Площадь;Население;Плотность населения;Преобладающая национальность;Развитая"
+            };
+
+            foreach (var country in Countries)
+            {
+                lines.Add($"{country.Name};{country.Capital};{country.Area};" +
+                    $"{country.Population};{country.Population_density};{country.MainNationality};" +
+                    $"{country.IsDeveloped};");
+            }
+            File.WriteAllLines(filePath, lines, System.Text.Encoding.UTF8);
+        }
+
+
+
+        public string GetStatisticsArea()
+        {
+            string result = "";
+
+            result += "----------ПЛОЩАДЬ---------- \r\n\r\n";
+
+            double maxArea = 0;
+            string maxAreaName = "";
+            foreach (Country country in Countries)
+            {
+                if (country.Area > maxArea)
+                {
+                    maxArea = country.Area;
+                    maxAreaName = country.Name;
+                }
+            }
+            result += $"Самая большая: {maxAreaName} ({maxArea} км²) \r\n";
+
+            double minArea = 999999999;
+            string minAreaName = "";
+            foreach (Country country in Countries)
+            {
+                if (country.Area < minArea && country.Area > 0)
+                {
+                    minArea = country.Area;
+                    minAreaName = country.Name;
+                }
+            }
+            result += $"Самая маленькая: {minAreaName} ({minArea} км²) \r\n\r\n";
+            return result;
+        }
+
+
+
+        public string GetStatisticsPopulation()
+        {
+            string result = "";
+
+            result += "----------НАСЕЛЕНИЕ----------\r\n\r\n";
+
+            int maxPopulation = 0;
+            string maxPopulationName = "";
+            foreach (Country country in Countries)
+            {
+                if (country.Population > maxPopulation)
+                {
+                    maxPopulation = country.Population;
+                    maxPopulationName = country.Name;
+                }
+            }
+            result += $"Самое большое население: {maxPopulationName} ({maxPopulation} чел.) \r\n";
+
+            int minPopulation = 999999999;
+            string minPopulationName = "";
+            foreach (Country country in Countries)
+            {
+                if (country.Population < minPopulation && country.Population > 0)
+                {
+                    minPopulation = country.Population;
+                    minPopulationName = country.Name;
+                }
+            }
+            result += $"Самое маленькое население: {minPopulationName} ({minPopulation} чел.) \r\n\r\n";
+            return result;
+        }
+
+
+        public string GetStatisticsDeveloped()
+        {
+            string result = "";
+
+            result += "----------РАЗВИТОСТЬ----------\r\n\r\n";
+            int developed = 0;
+            foreach (Country country in Countries)
+            {
+                if (country.IsDeveloped == true)
+                {
+                    developed++;
+                }
+            }
+            result += $"Развитых стран: {developed} из {Countries.Count}";
+
+            return result;
+        }
+
+
+        public void SortPopulationHighToLow()
+        {
+            List<Country> tempList = new List<Country>();
+
+            foreach (Country c in Countries)
+            {
+                tempList.Add(c);
+            }
+
+            for (int i = 0; i < tempList.Count - 1; i++)
+            {
+                for (int j = i + 1; j < tempList.Count; j++)
+                {
+                    if (tempList[i].Population < tempList[j].Population)
+                    {
+                        Country temp = tempList[i];
+                        tempList[i] = tempList[j];
+                        tempList[j] = temp;
+                    }
+                }
+            }
+
+            Countries.Clear();
+
+            foreach (Country c in tempList)
+            {
+                Countries.Add(c);
+            }
+        }
+
+
+
+        public void SortPopulationLowToHigh()
+        {
+            List<Country> tempList = new List<Country>();
+
+            foreach (Country c in Countries)
+            {
+                tempList.Add(c);
+            }
+
+            for (int i = 0; i < tempList.Count - 1; i++)
+            {
+                for (int j = i + 1; j < tempList.Count; j++)
+                {
+                    if (tempList[i].Population > tempList[j].Population)
+                    {
+                        Country temp = tempList[i];
+                        tempList[i] = tempList[j];
+                        tempList[j] = temp;
+                    }
+                }
+            }
+
+            Countries.Clear();
+
+            foreach (Country c in tempList)
+            {
+                Countries.Add(c);
+            }
+        }
+
+
+
+        public int FindCountryName(string countryName)
+        {
+            for (int i = 0; i < Countries.Count; i++)
+            {
+                if (Countries[i].Name.ToLower() == countryName.ToLower())
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        public void DevelopedFilter(bool OnlyDeveloped)
+        {
+            if (OnlyDeveloped)
+            {
+                if (originalCountries == null)
+                {
+                    originalCountries = new BindingList<Country>(Countries.ToList());
+                }
+
+                Countries.Clear();
+                foreach (var country in originalCountries)
+                {
+                    if (country.IsDeveloped)
+                    {
+                        Countries.Add(country);
+                    }
+                }
+            }
+            else
+            {
+                if (originalCountries != null)
+                {
+                    Countries.Clear();
+                    foreach (var country in originalCountries)
+                    {
+                        Countries.Add(country);
+                    }
+                    originalCountries = null;
+                }
+            }
+        }
+    }
+
+}
+    
+    
+        
+    
+
+
+
+
---
+++ Tyuiu.MertsKV.Sprint7.Project.V13.Test/DataServiseTest.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using Tyuiu.MertsKV.Sprint7.Project.V13.Lib;
+namespace Tyuiu.MertsKV.Sprint7.Project.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void LoadFromCSVTest()
+        {
+            DataService ds = new DataService();
+
+            string tempFile = Path.GetTempFileName();
+
+            File.WriteAllText(tempFile, "Название;Столица;Площадь;Население;Плотность;Национальность;Развитая\nРоссия;Москва;1000;100;1;Русские;true");
+
+            try
+            {
+                ds.LoadFromCsv(tempFile);
+
+                Assert.AreEqual(1, ds.Countries.Count);
+                Assert.AreEqual("Россия", ds.Countries[0].Name);
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+
+
+
+        [TestMethod]
+        public void GetStatistickAreaTest()
+        {
+            DataService ds = new DataService();
+
+            ds.Countries.Add(new Country { Name = "Россия", Area = 1000 });
+            ds.Countries.Add(new Country { Name = "Германия", Area = 200 });
+            ds.Countries.Add(new Country { Name = "Бразилия", Area = 800 });
+
+            string result = ds.GetStatisticsArea();
+
+            Assert.IsTrue(result.Contains("Самая большая: Россия"));
+            Assert.IsTrue(result.Contains("Самая маленькая: Германия"));
+        }
+
+
+
+        [TestMethod]
+        public void GetStatisticsPopulationTest()
+        {
+            DataService ds = new DataService();
+
+            ds.Countries.Add(new Country { Name = "Китай", Population = 1400000000 });
+            ds.Countries.Add(new Country { Name = "Россия", Population = 40000 });
+
+            string result = ds.GetStatisticsPopulation();
+
+            Assert.IsTrue(result.Contains("Китай"));
+            Assert.IsTrue(result.Contains("Россия"));
+        }
+
+
+
+        [TestMethod]
+        public void GetStatisticsDevelopedTest()
+        {
+
+            DataService ds = new DataService();
+            ds.Countries.Add(new Country { Name = "Россия", IsDeveloped = true });
+            ds.Countries.Add(new Country { Name = "Германия", IsDeveloped = true });
+            ds.Countries.Add(new Country { Name = "Бразилия", IsDeveloped = false });
+
+            string result = ds.GetStatisticsDeveloped();
+
+            Assert.IsTrue(result.Contains("Развитых стран: 2 из 3"));
+        }
+
+
+
+        [TestMethod]
+        public void SaveToCsvTest()
+        {
+            DataService ds = new DataService();
+            ds.Countries.Add(new Country
+            {
+                Name = "Россия",
+                Capital = "Москва",
+                Area = 100,
+                Population = 1000,
+                Population_density = 10,
+                MainNationality = "Русские",
+                IsDeveloped = true
+            });
+
+            string tempFile = Path.GetTempFileName();
+
+            try
+            {
+                ds.SaveToCsv(tempFile);
+
+                string content = File.ReadAllText(tempFile);
+                Assert.IsTrue(content.Contains("Россия"));
+                Assert.IsTrue(content.Contains("Москва"));
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+
+        [TestMethod]
+        public void SortPopulationHighToLowTest()
+        {
+            DataService ds = new DataService();
+
+            ds.Countries.Add(new Country { Name = "Россия", Population = 100 });
+            ds.Countries.Add(new Country { Name = "Бразилия", Population = 300 });
+            ds.Countries.Add(new Country { Name = "Германия", Population = 200 });
+
+            ds.SortPopulationHighToLow();
+
+            Assert.AreEqual("Бразилия", ds.Countries[0].Name);
+            Assert.AreEqual("Германия", ds.Countries[1].Name);
+            Assert.AreEqual("Россия", ds.Countries[2].Name);
+        }
+
+        [TestMethod]
+        public void SortPopulationLowToHighTest()
+        {
+            DataService ds = new DataService();
+
+            ds.Countries.Add(new Country { Name = "Россия", Population = 300 });
+            ds.Countries.Add(new Country { Name = "Германия", Population = 100 });
+            ds.Countries.Add(new Country { Name = "Бразилия", Population = 200 });
+
+            ds.SortPopulationLowToHigh();
+
+            Assert.AreEqual("Германия", ds.Countries[0].Name);
+            Assert.AreEqual("Бразилия", ds.Countries[1].Name);
+            Assert.AreEqual("Россия", ds.Countries[2].Name);
+        }
+
+        [TestMethod]
+        public void FindCountryNameTest()
+        {
+            DataService ds = new DataService();
+
+            ds.Countries.Add(new Country { Name = "Россия" });
+            ds.Countries.Add(new Country { Name = "Германия" });
+            ds.Countries.Add(new Country { Name = "Бразилия" });
+
+            int index = ds.FindCountryName("Германия");
+
+            Assert.AreEqual(1, index);
+        }
+
+        [TestMethod]
+        public void DevelopedFilter_OnlyDevelopedTrue_FiltersCorrectly()
+        {
+            DataService ds = new DataService();
+
+            ds.Countries.Add(new Country { Name = "Россия", IsDeveloped = true });
+            ds.Countries.Add(new Country { Name = "Бразилия", IsDeveloped = false });
+            ds.Countries.Add(new Country { Name = "Япония", IsDeveloped = true });
+
+            ds.DevelopedFilter(true);
+
+            Assert.AreEqual(2, ds.Countries.Count);
+        }
+    }
+
+
+}
+
+
+
+
